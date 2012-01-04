540258b52c52ddf3e1b3ba6b3733777b530b9fae
Ramon <oni.pool@gmail.com>
2012-01-04 23:13:15 +0200

    Append the textarea instead of replacing the first child

--- js/impress_editor.js
+++ js/impress_editor.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
         $("div.step").click(function(eventObject) {
             // Replace the contents of the current slide with its own html to be edited
             var ownHtml = $(this).html();
-            var thisChild = $(this).children(":first");
-            $(thisChild).replaceWith('<textarea columns="20">' +  ownHtml + '</textarea>');
+            $(this).append('<textarea>' +  ownHtml + '</textarea>');
 
             // Disable click handle
             $(this).off("click");
