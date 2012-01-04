0eb03a9726415974e85d7e6a584e6cbe8a16cd96
Ramon <oni.pool@gmail.com>
2012-01-05 00:26:37 +0200

    Added save button and disable key navigation when editing

--- js/impress.js
+++ js/impress.js
@@ -256,7 +256,8 @@
     
     // EVENTS
     
-    document.addEventListener("keydown", function ( event ) {
+    document.addEventListener("keydown", filterKeys, false);
+    function filterKeys( event ) {
         if ( event.keyCode == 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
             var next = active;
             switch( event.keyCode ) {
@@ -278,9 +279,11 @@
             
             select(next);
             
-            event.preventDefault();
+            //event.preventDefault();
+            return false;
         }
-    }, false);
+    }
+    document.filterKeys = filterKeys;
 
     document.addEventListener("click", function ( event ) {
         // event delegation with "bubbling"
